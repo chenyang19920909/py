60e3ceb7c46e054a6be3b6fca01508a6c31f28ca
chenyang <295568526@qq.com>
2018-10-09 13:54:02 +0800

    修改程序错误问题：写文件未关闭时，send_mail导致邮件内容为空

--- runtest.py
+++ runtest.py
@@ -12,7 +12,8 @@ from email.mime.multipart import MIMEMultipart
 
 #定义邮件发送
 def send_mail(file_new):
-    with open(file_new, 'rb') as f:
+    #print("file_new=" + file_new)
+    with open(file, 'rb') as f:
         mail_body = f.read()
 
         # msg = MIMEText(mail_body, 'base64', 'utf-8')
@@ -25,7 +26,7 @@ def send_mail(file_new):
         msg.attach(MIMEText("测试结果详见附件", 'html', 'utf-8'))
         msg['Subject'] = Header("自动化测试报告" + now, 'utf-8')
         msg.attach(att)
-        print(mail_body)
+        #print(mail_body)
 
         smtp = smtplib.SMTP()
         smtp.connect("smtp.1218.com.cn")
@@ -38,23 +39,25 @@ def send_mail(file_new):
 def new_report(testreport):
     lists = os.listdir(testreport)
     lists.sort(key=lambda fn: os.path.getmtime(testreport + '\\' + fn))
-    file_new = os.path.join(testreport, lists[-1]).replace("\\t", "\\\\t")
-    print(file_new)
+    file_new = os.path.join(testreport, lists[-1])
+    #print(file_new)
     return file_new
 
 if __name__ == '__main__':
     test_dir = "./test_case"
-    test_report = "D:\py\\test_python_selenium\\test_project\\report"
+    test_report = "D:\\py\\test_python_selenium\\test_project\\report"
 
     discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
 
     now = time.strftime("%Y-%m-%d %H_%M_%S")
     # 生成HTML测试报告。定义报告存放路径
     file = test_report + '\\' + now + 'result.html'
+    #print("file" + file)
     with open(file, 'wb') as fp:
         runner = HTMLTestRunner(stream=fp, title='测试报告', description='用例执行情况:')
         runner.run(discover)
 
-        new_report = new_report(test_report)
-        print(new_report)
-        send_mail(new_report)
+    # new_report = new_report(test_report)
+    # print(new_report)
+    # send_mail(new_report)
+    send_mail(file)
